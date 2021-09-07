a38b35fcfa4dbfab73410f80c4e170e18c8d2dad
Ricardo Alvarez <ric.alv_17@hotmail.com>
2021-09-07 09:29:56 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IActionMapping, ITreeOptions, TreeNode, TREE_ACTIONS } from '@circlon/angular-tree-component';
+import { ContextMenuComponent } from 'ngx-contextmenu';
+import { ContextMenuService } from 'ngx-contextmenu';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  actionMapping: IActionMapping = {
+    mouse: {
+      click: (tree, node, $event) => {
+        if (node.hasChildren) {
+          TREE_ACTIONS.TOGGLE_EXPANDED(tree, node, $event);
+        }else{
+          console.log(node.data.url);
+        }
+      },
+      contextMenu: (tree, node, $event) => {
+        $event.preventDefault();
+        this.onContextMenu($event, node.data);
+      }
+    }
+}
+
   title = 'ng-tree';
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1', url: 'hola1' },
+        { id: 3, name: 'child2', url: 'hola2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1', url: 'hola2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub', url: 'hola2.2' }
+          ]
+        }
+      ]
+    }
+  ];
+  options: ITreeOptions = { actionMapping: this.actionMapping };
+
+  public items = [
+    { name: 'John', otherProperty: 'Foo' },
+    { name: 'Joe', otherProperty: 'Bar' }
+  ];
+
+  @ViewChild(ContextMenuComponent, { static: true }) public basicMenu: ContextMenuComponent;
+
+
+  constructor(private contextMenuService: ContextMenuService){}
+
+  public onContextMenu($event: MouseEvent, item: any): void {
+    this.contextMenuService.show.next({
+      // Optional - if unspecified, all context menu components will open
+      contextMenu: this.basicMenu,
+      event: $event,
+      item: item,
+    });
+
+    $event.preventDefault();
+    $event.stopPropagation();
+  }
+
+  showMessage(message: any) {
+    console.log(message);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TreeModule } from '@circlon/angular-tree-component';
+import { ContextMenuModule } from 'ngx-contextmenu';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TreeModule,
+    ContextMenuModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
